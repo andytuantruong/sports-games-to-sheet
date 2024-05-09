32219a8e0558ad05070fe80ccd3882575d4b3b12
andytuantruong <andytuantruong@gmail.com>
2024-05-08 22:17:36 -0700

    view all info in game_elements scraped and specify away/home team abbreviation

--- scraper.py
+++ scraper.py
@@ -16,13 +16,30 @@ game_elements = driver.find_elements(
     By.XPATH, 
     "//div[contains(@class, 'lineup is-mlb') "
     "and not(@class='lineup is-mlb is-tools') "
-    "and not(@class='lineup is-mlb is-tools is-deposit-offer')]"
+    "and not(@class='lineup is-mlb is-tools is-deposit-offer') "
+    "and not (@class='lineup is-ad hide-until-lg')]"
 )
 
-away_team = driver.find_element(By.XPATH,"/html/body/div/div/main/div[3]/div[1]/div[2]/div[1]/div/div[1]/div")
-home_team = driver.find_element(By.XPATH,"/html/body/div/div/main/div[3]/div[1]/div[2]/div[1]/div/div[2]/div")
+# Print full list of info from each game box
+for index, game_element in enumerate(game_elements, start=1):
+    print("Game Element", index)
+    print("Tag Name:", game_element.tag_name)
+    print("Text Content:", game_element.text)
+    print("Class:", game_element.get_attribute('class'))
+    print("-" * 100)
 
-print("Away Team:", away_team.text)
-print("Home Team:", home_team.text)
-print("Games played today:", len(game_elements))
-driver.quit()
+game_index = 1
+for game_element in game_elements:
+    lines = game_element.text.split('\n')
+
+    away_team_abbrev = lines[3]
+    home_team_abbrev = lines[4]
+
+    print("Game", game_index)
+    print("Away Team Abbreviation:", away_team_abbrev)
+    print("Home Team Abbreviation:", home_team_abbrev)
+    print()
+
+    game_index += 1
+
+driver.close()
